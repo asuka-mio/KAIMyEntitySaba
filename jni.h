38ea2b7b2753733bed31db1a69ac00848e2b07c6
asuka-mio <yuanqingxiang233@163.com>
2021-09-11 19:13:36 +0800

    init

---
+++ jni.h
@@ -0,0 +1,6 @@
+#ifdef WIN32
+#include "jni_win32.h"
+#endif
+#ifdef linux
+#include "jni_linux.h"
+#endif
---
+++ jni_md.h
@@ -0,0 +1,6 @@
+#ifdef WIN32
+#include "jni_md_win32.h"
+#endif
+#ifdef linux
+#include "jni_md_linux.h"
+#endif
